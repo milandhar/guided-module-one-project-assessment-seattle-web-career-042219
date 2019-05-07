ce9d2f72c47053b85f1cd68aef416e8ab7e14a11
Milan <milan.dhar@gmail.com>
2019-05-07 14:21:26 -0700

    deleted topic model for now

--- lib/article.rb
+++ lib/article.rb
@@ -1,5 +1,4 @@
 class Article < ActiveRecord::Base
   has_many :users, through: :user_articles
   has_many :users
-  belongs_to :topic
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
   has_many :user_articles
   has_many :articles, through: :user_articles
-  has_many :topics 
 end
